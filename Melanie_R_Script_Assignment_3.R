9ea1e526c8057ce3df0efd146ad0c6b2c8211289
jacquelineliz <jwu22@uoguelph.ca>
2021-11-17 21:27:37 -0500

    Last updated November 17 2021

--- Melanie_R_Script_Assignment_3.R
+++ Melanie_R_Script_Assignment_3.R
@@ -3,19 +3,19 @@
 #Questions of interest: What type of habitat do Coccinellidae thrive the most in? Is there a correlation with most inhabited habitat type and most inhabited world location?
 
 #Attaching libraries----
+library(dplyr)
 library(mapproj)
 library(oce)
-library(dplyr)
 library(tidyverse)
 library(vegan)
 
 #Loading the Data----
 
 #Obtaining data
-dfCoccinellidae<-read_tsv("http://www.boldsystems.org/index.php/API_Public/combined?taxon=Coccinellidae&format=tsv")
+#dfCoccinellidae<-read_tsv("http://www.boldsystems.org/index.php/API_Public/combined?taxon=Coccinellidae&format=tsv")
 
 #Exporting data to file. (For reproducibility and )
-write.csv(dfCoccinellidae, "dfCoccinellidae.csv")
+#write.csv(dfCoccinellidae, "dfCoccinellidae.csv")
 
 #Exploring the Data----
 
@@ -28,61 +28,64 @@ is.na(dfCoccinellidae)
 #From what countries do majority of the data come from?
 names<-unique(dfCoccinellidae$country)
 
-#Viewing the countries to create manual input of countries by continents.
+#Viewing the countries to create manual input of countries by continents (to a personal database).
 print(names)
 
-#Creating a vector for each continent
-North_America<-c("Canada", "United States", "Greenland", "Costa Rica", "Mexico", "Guatemala", "Honduras", "Panama", "Puerto Rico")
-
-Europe<-c("Germany", "Spain", "Finland", "Italy", "Norway", "United Kingdom", "Poland", "Portugal", "Moldova", "Greece", "Czech Republic", "Sweden", "Switzerland", "Bulgaria", "France", "Belgium", "Netherlands", "Belarus", "Slovakia", "Ukraine", "Estonia", "Austria", "Slovenia")
-
-Asia<-c("Malaysia", "Russia", "Bangladesh", "Pakistan", "Indonesia", "India", "Japan", "Thailand", "China", "Philippines", "Saudi Arabia", "Vietnam", "Israel", "Oman", "Iran", "Taiwan", "Syria", "Uzbekistan", "Laos", "Turkey", "Sri Lanka", "Armenia", "Myanmar", "South Korea")
-
-South_America<-c("Venezuela", "Argentina", "Ecuador", "Brazil", "Bolivia", "Chile")
-
-Oceania<-c("Australia", "French Polynesia", "Papua New Guinea", "New Zealand", "West Papua", "New Caledonia", "Solomon Islands")
-
-Africa<-c("South Africa", "Gabon", "Madagascar", "Egypt", "Zambia", "Kenya", "Cameroon", "Ethiopia", "Uganda", "Tanzania", "Rwanda", "Senegal")
+#Reading-in a personal database of continents and countries
+continent_library_file<-file.choose()
+continent_library<-read.csv(continent_library_file, sep = ";")
 
 #Creating continent vector
 continents<-c()
+North_America_count<-c()
+South_America_count<-c()
+Asia_count<-c()
+Oceania_count<-c()
+Europe_count<-c()
+Africa_count<-c()
 for(i in 1:nrow(dfCoccinellidae)){
   value_check<-0
   if(is.na(dfCoccinellidae$country) == FALSE){
-    for(j in 1:length(North_America)){
-      if(identical(dfCoccinellidae$country[i], North_America[j])){
+    for(j in 1:length(continent_library$North_America)){
+      if(identical(dfCoccinellidae$country[i], continent_library$North_America[j])){
         continents<-c(continents, "North_America")
         value_check<-1
+        North_America_count<-North_America_count+1
       }
     }
-    for(j in 1:length(Europe)){
-      if(identical(dfCoccinellidae$country[i], Europe[j])){
+    for(j in 1:length(continent_library$Europe)){
+      if(identical(dfCoccinellidae$country[i], continent_library$Europe[j])){
         continents<-c(continents, "Europe")
         value_check<-1
+        Europe_count<-Europe_count+1
       }
     }
-    for(j in 1:length(Asia)){
-      if(identical(dfCoccinellidae$country[i], Asia[j])){
+    for(j in 1:length(continent_library$Asia)){
+      if(identical(dfCoccinellidae$country[i], continent_library$Asia[j])){
         continents<-c(continents, "Asia")
         value_check<-1
+        Asia_count<-Asia_count+1
       }
     }
-    for(j in 1:length(South_America)){
-      if(identical(dfCoccinellidae$country[i], South_America[j])){
+    for(j in 1:length(continent_library$South_America)){
+      if(identical(dfCoccinellidae$country[i], continent_library$South_America[j])){
         continents<-c(continents, "South_America")
         value_check<-1
+        South_America_count<-South_America_count+1
       }
     }
-    for(j in 1:length(Oceania)){
-      if(identical(dfCoccinellidae$country[i], Oceania[j])){
+    for(j in 1:length(continent_library$Oceania)){
+      if(identical(dfCoccinellidae$country[i], continent_library$Oceania[j])){
         continents<-c(continents, "Oceania")
         value_check<-1
+        Oceania_count<-Oceania_count+1
       }
     }
-    for(j in 1:length(Africa)){
-      if(identical(dfCoccinellidae$country[i], Africa[j])){
+    for(j in 1:length(continent_library$Africa)){
+      if(identical(dfCoccinellidae$country[i], continent_library$Africa[j])){
         continents<-c(continents, "Africa")
         value_check<-1
+        Africa_count<-Africa_count+1
       }
     }
   }
@@ -96,13 +99,59 @@ for(i in 1:nrow(dfCoccinellidae)){
   #Provides leeway for changes in the data
   if(value_check == 0){
     print(sprintf("Continent of %s not found", dfCoccinellidae$country[i]))
-    manual_input<-readline(prompt = "Please enter continent manually:")
+    manual_input<-readline(prompt = "Please enter continent manually: ")
     continents<-c(continents, manual_input)
+    
+    if(identical(manual_input, "North_America")){
+      continent_library_North_America_no_blanks = continent_library$North_America[continent_library$North_America != ""]
+      if(length(continent_library$North_America) > length(continent_library_North_America_no_blanks)){
+        continent_library$North_America[(length(continent_library_North_America_no_blanks) + 1)] = dfCoccinellidae$country[i]
+      }
+    }
+    if(identical(manual_input, "South_America")){
+      continent_library_South_America_no_blanks = continent_library$South_America[continent_library$South_America != ""]
+      if(length(continent_library$South_America) > length(continent_library_South_America_no_blanks)){
+        continent_library$South_America[(length(continent_library_South_America_no_blanks) + 1)] = dfCoccinellidae$country[i]
+      }
+    }
+    if(identical(manual_input, "Europe")){
+      eur_length<-length(continent_library$Europe)
+      continent_library_Europe_no_blanks = c(continent_library$Europe[continent_library$Europe != ""], dfCoccinellidae$country[i])
+      if(length(continent_library$Europe) > length(continent_library_Europe_no_blanks)){
+        continent_library$Europe[(length(continent_library_Europe_no_blanks) + 1)] = dfCoccinellidae$country[i]
+      }
+    }
+    if(identical(manual_input, "Oceania")){
+      continent_library_Oceania_no_blanks = c(continent_library$Oceania[continent_library$Oceania != ""], dfCoccinellidae$country[i])
+      if(length(continent_library$Oceania) > length(continent_library_Oceania_no_blanks)){
+        continent_library$Oceania[(length(continent_library_Oceania_no_blanks) + 1)] = dfCoccinellidae$country[i]
+      }
+    }
+    if(identical(manual_input, "Asia")){
+      asi_length<-length(continent_library$Asia)
+      continent_library_Asia_no_blanks = c(continent_library$Asia[continent_library$Asia != ""], dfCoccinellidae$country[i])
+      if(length(continent_library$Asia) > length(continent_library_Asia_no_blanks)){
+        continent_library$Asia[(length(continent_library_Asia_no_blanks) + 1)] = dfCoccinellidae$country[i]
+      }
+    }
+    if(identical(manual_input, "Africa")){
+      continent_library_Africa_no_blanks = c(continent_library$Africa[continent_library$Africa != ""], dfCoccinellidae$country[i])
+      if(length(continent_library$Africa) > length(continent_library_Africa_no_blanks)){
+        continent_library$Africa[(length(continent_library_Africa_no_blanks) + 1)] = dfCoccinellidae$country[i]
+      }
+    }
+            
+    #Updating the personal database
+    write.csv(x = continent_library, file = paste("Personal_db_Continents_and_Countries_updated", Sys.Date(), collapse = "", sep = ""), quote = FALSE, row.names = FALSE)
   }
 }
 
-View(continents)
-View(dfCoccinellidae$country)
+continent_library$North_America
+continent_library$Europe
+continent_library$Africa
+continent_library$Asia
+continent_library$South_America
+continent_library$Oceania
 
 ###Diversity Question 1:BIN based Question ----
 dfCoccinellidae$bin_uri
@@ -126,13 +175,11 @@ dfCount_by_BIN
 dfCount_by_BIN2<-na.omit(dfCount_by_BIN)
 dfCount_by_BIN2
 
-#my x axis
 col1_dfCount_by_BIN2<-dfCount_by_BIN2[ , 1]
 col1_dfCount_by_BIN2
 dim(col1_dfCount_by_BIN2)
 class(col1_dfCount_by_BIN2)
 
-#my y axis
 dfCoccinellidae$country
 unique(dfCoccinellidae$country)
 
@@ -158,8 +205,8 @@ coord <- mapproject(dfCoccinellidae$lon, dfCoccinellidae$lat, proj="gilbert", or
 points(coord, pch=20, cex=1.2, col="red")  #plot converted points
 
 #world map
-maps::map(database= "world", col="grey80", fill=TRUE, projection="gilbert")
-coord <- mapproject(dfCoccinellidae$lon, dfCoccinellidae$lat, proj="gilbert")  #convert points to projected lat/long
+maps::map(database= "world", col="grey80", fill=TRUE, projection="mercator")
+coord <- mapproject(dfCoccinellidae$lon, dfCoccinellidae$lat, proj="mercator")  #convert points to projected lat/long
 points(coord, pch=20, cex=1.2, col="red")  #plot converted points
 
 #End of Assignment 1
